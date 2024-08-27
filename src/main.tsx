3e0b1e11cf53e136ec686ba900bf8a95f37a2de9
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-08-27 06:24:52 +0200

    Update react-router-dom import to BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "./miracle-ui.css";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
